9b580be63d2df042bcdd94a901c957346230ca57
dongwoo <73861795+ornwoo96@users.noreply.github.com>
2025-02-12 00:58:51 +0900

    퀴즈타이머 align 셋업

--- src/screens/MainTabs/Home/ListItems/QuizTimer.tsx
+++ src/screens/MainTabs/Home/ListItems/QuizTimer.tsx
@@ -44,6 +44,7 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderColor: "lightgray",
         backgroundColor: "white",
+        alignSelf: "center",
     },
     quizIcon: {
         width: 27.5,
